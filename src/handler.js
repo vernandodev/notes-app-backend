91dc4c6148309c4b000ba63ad41d60149c3344cd
Richovrnndo <Richovrnando@gmail.com>
2022-06-21 16:26:21 +0700

    update

---
+++ src/handler.js
@@ -0,0 +1,5 @@
+const addNoteHandler = (request, h) => {
+
+};
+
+export default { addNoteHandler };
---
+++ src/server.js
@@ -0,0 +1,13 @@
+import Hapi from "@hapi/hapi";
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: "localhost",
+    });
+
+    await server.start();
+    console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+init();

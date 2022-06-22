a65119802435f6c900e9ca7c9d1fa6e11ca2080f
Richovrnndo <Richovrnando@gmail.com>
2022-06-22 10:26:11 +0700

    update

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,32 @@
-const addNoteHandler = (request, h) => {
+import { nanoid } from 'nanoid';
+import notes from './notes';
 
+const addNoteHandler = (request, h) => {
+    const { title, tags, body } = request.payload;
+
+    const id = nanoid(16);
+    const createdAt = new Date().toString();
+    const updatedAt = createdAt;
+
+    const newNotes = {
+        title, tags, body, id, createdAt, updatedAt,
+    };
+
+    notes.push(newNotes);
+
+    const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'catatan berhasil ditambahkan',
+            data: {
+                noteId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
 };
 
 export default { addNoteHandler };

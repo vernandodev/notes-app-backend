5f31320384d1c6ffe31a057583e55f774a0b1d45
Richovrnndo <Richovrnando@gmail.com>
2022-06-27 13:27:35 +0700

    update

--- src/handler.js
+++ src/handler.js
@@ -45,4 +45,83 @@ export const getAllNotesHandler = () => ({
     }
 });
 
+export const getNoteByIdHandler = (request, h) => {
+    const {id} = request.params;
+
+    const note = notes.filter((n) => n.id === id)[0];
+
+    if (note !== undefined) {
+        return {
+            status: 'success',
+            data: {
+                note,
+            },
+        };
+    }
+
+    const response = h.response({
+        status: 'gagal',
+        message: 'Catatan tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+};
+
+export const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params; // kita dapatkan dulu nilai id yang dikirim melalui path parameters.
+
+    const { title, tags, body } = request.payload;
+    const updatedAt = new Date().toISOString();
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt,
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil diperbarui',
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'gagal',
+        message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
+export const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params; // kita dapatkan dulu nilai id yang dikirim melalui path parameters.
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+        notes.splice(index, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil dihapus',
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'gagal',
+        message: 'Catatan gagal dihapus. id tidak ditemukan'
+    });
+    response.code(404);
+    return response;
+}
+
 export default addNoteHandler;
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,37 @@
-import addNoteHandler, {getAllNotesHandler} from './handler.js';
+import addNoteHandler, {
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler
+}
+from './handler.js';
 
 const routes = [
     {
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler,
+      method: 'POST',
+      path: '/notes',
+      handler: addNoteHandler,
     },
     {
       method: 'GET',
       path: '/notes',
       handler: getAllNotesHandler,
     },
+    {
+      method: 'GET',
+      path: '/notes/{id}',
+      handler: getNoteByIdHandler,
+    },
+    {
+      method: 'PUT',
+      path: '/notes/{id}',
+      handler: editNoteByIdHandler,
+    },
+    {
+      method: 'DELETE',
+      path: '/notes/{id}',
+      handler: deleteNoteByIdHandler,
+    }
 
 ];
 
